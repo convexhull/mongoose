6d97d8deac5b74b9aac21d856522bf1548a4d47e
Yash Pratap Singh <singhyashpratap06@gmail.com>
2019-01-23 11:25:07 +0530

    added populate feature

--- getting-started.js
+++ getting-started.js
@@ -11,37 +11,49 @@ db.on("error", function() {
 db.once("connected", function() {
   console.log("Connected to MongoDB database");
 });
+const Schema = mongoose.Schema;
 
-var blogSchema = new mongoose.Schema({
-  title:  String,
-  author: String,
-  body:   String,
-  comments: [{ body: String, date: {type:Date, default : Date.now}}],
-  date: { type: Date, default: Date.now },
-  hidden: Boolean,
-  meta: {
-    votes: Number,
-    favs:  Number
-  }
+const personSchema = Schema({
+  _id: Schema.Types.ObjectId,
+  name: String,
+  age: Number,
+  stories: [{ type: Schema.Types.ObjectId, ref: 'Story' }]
 });
 
-blogSchema.methods.show = function() {
-  console.log(this.comments);
-}
-
-var Blog = mongoose.model("Blog",blogSchema);
-
-
-
-var first = new Blog({
-  title : "EngiGyan",
-  author : "Yash",
-  comments : [{body:"wow"}],
-  meta : {
-    votes : 35,
-    favs : 2
-  }
+const storySchema = Schema({
+  author: { type: Schema.Types.ObjectId, ref: 'Person' },
+  title: String,
+  fans: [{ type: Schema.Types.ObjectId, ref: 'Person' }]
 });
 
-first.save();
+const Story = mongoose.model("Story", storySchema);
+const Person = mongoose.model("Person", personSchema);
+
+const author = new Person({
+  _id: new mongoose.Types.ObjectId(),
+  name: 'Ian Fleming',
+  age: 50
+});
+
+author.save(function (err) {
+  if (err) return handleError(err);
+
+  const story1 = new Story({
+    title: 'Casino Royale',
+    author: author._id    // assign the _id from the person
+  });
+
+  story1.save(function (err) {
+    if (err) return handleError(err);
+    // thats it!
+  });
+});
+
+Story.
+  findOne({ title: 'Casino Royale' }).
+  populate('author').
+  exec(function (err, story) {
+      console.log(story);
+    // prints "The author is Ian Fleming"
+  });
 
